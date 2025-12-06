4b919ae73942b3dfdebb5de1f72fe5f18845aeab
dawid <dawid.poradzinski@microsoft.wsei.edu.pl>
2025-12-06 13:42:38 +0100

    new commit with docker-compose

---
+++ initial/src/main/java/com/example/restservice/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.restservice;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByName(String name);
+}
